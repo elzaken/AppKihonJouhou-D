3c4766ea6fe9a9a4784df4403db65023bfcc039e
Naoyuki Ito <naoyuki.ito.sys@gmail.com>
2017-07-11 11:16:19 +0900

    問２です

--- src/KihonJouhou.java
+++ src/KihonJouhou.java
@@ -28,6 +28,17 @@ public static void main(String[] args){
 
 	public static int situmon2(){
 		int answer = 0;
+		Scanner sc2 = new Scanner(System.in);
+		System.out.println("問題２：公衆回線をあたかも自社内で構築した"
+				+ "ネットワークのような使い勝手で利用できるサービスは？");
+		System.out.println("選択肢１～４の中から正解を選んでください。");
+		System.out.println("１：広域イーサネット");
+		System.out.println("２：WAN");
+		System.out.println("３：ISDN");
+		System.out.println("４：VPN");
+		System.out.println("入力：");
+		answer = sc2.nextInt();
+		System.out.println("正解は：４です");
 		return answer;
 
 	}
